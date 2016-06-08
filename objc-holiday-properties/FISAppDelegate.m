a6b7f11dd934a2994d8f3a96f4cf41d2b3e43372
chriswebb09 <chris.webb5249@gmail.com>
2016-06-08 19:28:50 -0400

    Done.

--- objc-holiday-properties/FISAppDelegate.m
+++ objc-holiday-properties/FISAppDelegate.m
@@ -9,32 +9,42 @@
     return YES;
 }
 
+
 - (NSArray *)holidaysInSeason:(NSString *)season {
-    
-    return nil;
+    return self.database[season];
 }
 
 - (NSArray *)suppliesInHoliday:(NSString *)holiday
                       inSeason:(NSString *)season {
-    
-    return nil;
+    return self.database[season][holiday];
 }
 
 - (BOOL)holiday:(NSString* )holiday
      isInSeason:(NSString *)season {
     
-    return nil;
+    if ([[self.database[season] allKeys] containsObject:holiday]) {
+        return YES;
+    }
+    return NO;
 }
 
 - (BOOL)supply:(NSString *)supply
    isInHoliday:(NSString *)holiday
       inSeason:(NSString *)season {
-    
-    return nil;
+    if ([self.database[season][holiday] containsObject:supply]) {
+        
+        return YES;
+        
+    }
+    return NO;
 }
+
+
 
 - (void)addHoliday:(NSString *)holiday
           toSeason:(NSString *)season {
+    
+    [self.database[season] setValue:@[] forKey:holiday];
     
     // no return
 }
@@ -42,8 +52,44 @@
 - (void)addSupply:(NSString *)supply
         toHoliday:(NSString *)holiday
          inSeason:(NSString *)season {
+    [self.database[season][holiday] addObject:supply];
+
+}
+
+
+- (BOOL)holiday:(NSString *)holiday
+     isInSeason:(NSString *)season
+     inDatabase:(NSDictionary *)database {
     
-    // no return
+    NSArray *keys = [self.database[season] allKeys];
+    if ([keys containsObject:holiday]) {
+        
+        return YES;
+        
+    }
+    return NO;
+    
+}
+
+- (NSDictionary *)addHoliday:(NSString *)holiday
+                    toSeason:(NSString *)season
+                  inDatabase:(NSDictionary *)database {
+    
+    NSMutableArray *holidayArray = [[NSMutableArray alloc] init];
+    [self.database[season] setValue:holidayArray forKey:holiday];
+    return self.database;
+    
+}
+
+
+- (NSDictionary *)addSupply:(NSString *)supply
+                  toHoliday:(NSString *)holiday
+                   inSeason:(NSString *)season
+                 inDatabase:(NSDictionary *)database {
+    
+    [self.database[season][holiday] addObject:supply];
+    return self.database;
+    
 }
 
 @end
